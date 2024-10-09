fa478a4f3ce14c6f48eb36914d3be5c0c009b42b
Paulo Aguiar <pvga@cesar.school>
2024-10-09 14:43:25 -0300

    Jaz aqui o arquivo referente ao cálculo de média

---
+++ Notasalunopython.py
@@ -0,0 +1,5 @@
+num1= float (input("Escreva a primeira nota do um aluno: "))
+num2= float (input("Escreva a segunda nota do aluno: "))
+num3= float (input("Escreva as terceira nota de um aluno: "))
+
+print ("A média do aluno foi de: ", (num1 + num2 + num3)/3)
